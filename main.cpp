4e72a1d1fde323798195165bff2922ce9349b35f
taekahn <ennuity@gmail.com>
2025-07-07 10:49:54 -0400

    ??? no idea what the point of this was

--- main.cpp
+++ main.cpp
@@ -1,18 +1,59 @@
-#ifndef F_CPU
-#define F_CPU 16000000UL
-#endif
+#include <inttypes.h>
 
-#include <avr/io.h>
-#include <util/delay.h>
-
-int main(void)
+class Debouncer
 {
-  DDRC = 0xFF;
-  while (1)
+  unsigned long previous_millis;
+  uint16_t interval_millis;
+  uint8_t state;
+  unsigned long stateChangeLastTime;
+  unsigned long durationOfPreviousState;
+};
+
+
+class Bounce : public Debouncer
+{
+  uint8_t pin;
+};
+
+struct myButton {
+  Bounce BtnBounce;
+  long buttonPressTimeStamp;
+  long buttonReleaseTimeStamp;
+  int buttonNumber;                 //Physical position on the board
+  int ArduinoPin {8};
+  int ShiftRegisterLEDPin[2] {8,8};
+  bool buttonPressed {false};       //Was the button Pressed during the loop
+  bool colorStatus[2] {0, 0};   //2 because 2 color per button
+  bool BtnLongPress {false};
+};
+
+constexpr static int NUM_BUTTONS = 10;
+constexpr static int NUM_PAGES = 4;
+
+
+struct myPage {
+  struct myButton ListObjButton[NUM_BUTTONS];
+  int pageNumber {99};
+  int HXmode {98};
+};
+
+
+
+struct myPage ListObjPage[NUM_PAGES];
+/*
+template <int T>
+struct size;
+
+size<sizeof(ListObjPage)> size_in_bytes;
+*/
+
+int main()
+{
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+  while (true)
   {
-    PORTC = 0xFF;
-    _delay_ms(1000);
-    PORTC = 0x00;
-    _delay_ms(1000);
+
   }
+#pragma clang diagnostic pop
 }
